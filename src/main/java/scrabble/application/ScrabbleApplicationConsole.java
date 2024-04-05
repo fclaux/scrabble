afb731cfddb0e0704adcab22800e07917e5d60ab
Maxence Vincent <maxvince87@gmail.com>
2024-04-05 09:27:37 +0200

    add line in main

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -5,6 +5,7 @@ public class ScrabbleApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("---------------------------------------------------------");
 		System.out.println("--  Bienvenue dans notre magnifique jeu de scrabble !  --");
+		System.out.println("--  développé par Maxence                              --");
 		System.out.println("---------------------------------------------------------");
 
 	}
