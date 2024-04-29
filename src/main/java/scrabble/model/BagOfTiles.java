e4969fa8290652f8df71e1d5d4acf38ff6ddde91
Hashual <145881009+Hashual@users.noreply.github.com>
2024-04-29 16:28:01 +0200

    Fix the bag of tile to have the great number of each letters

--- src/main/java/scrabble/model/BagOfTiles.java
+++ src/main/java/scrabble/model/BagOfTiles.java
@@ -15,31 +15,45 @@ public class BagOfTiles {
     }
 
     private void initializeTiles() {
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 15;i++) {
+        	tiles.add(new Tile(Letters.E));	
+        }
+    	for (int i = 0; i < 9; i++) {
             tiles.add(new Tile(Letters.A));
         }
-        for (int i = 0; i < 2; i++) {
-            tiles.add(new Tile(Letters.B));
-            tiles.add(new Tile(Letters.C));
-            tiles.add(new Tile(Letters.D));
-            tiles.add(new Tile(Letters.F));
-            tiles.add(new Tile(Letters.G));
-            tiles.add(new Tile(Letters.H));
-            tiles.add(new Tile(Letters.J));
-            tiles.add(new Tile(Letters.L));
-            tiles.add(new Tile(Letters.M));
+    	for (int i = 0; i < 8; i++) {
+            tiles.add(new Tile(Letters.I));
+        }
+    	for (int i = 0; i < 6; i++) {
             tiles.add(new Tile(Letters.N));
-            tiles.add(new Tile(Letters.P));
-            tiles.add(new Tile(Letters.Q));
+            tiles.add(new Tile(Letters.O));
             tiles.add(new Tile(Letters.R));
             tiles.add(new Tile(Letters.S));
             tiles.add(new Tile(Letters.T));
             tiles.add(new Tile(Letters.U));
-            tiles.add(new Tile(Letters.V));
         }
-        tiles.add(new Tile(Letters.E));
-        tiles.add(new Tile(Letters.I));
-        tiles.add(new Tile(Letters.O));
+    	
+    	for (int i = 0; i < 5; i++) {
+            tiles.add(new Tile(Letters.L));
+    	}
+    	
+    	for (int i = 0; i < 3; i++) {
+            tiles.add(new Tile(Letters.D));
+            tiles.add(new Tile(Letters.M));
+    	}
+    	
+    	for (int i = 0; i < 2; i++) {
+            tiles.add(new Tile(Letters.G));
+            tiles.add(new Tile(Letters.B));
+            tiles.add(new Tile(Letters.C));
+            tiles.add(new Tile(Letters.P));
+            tiles.add(new Tile(Letters.F));
+            tiles.add(new Tile(Letters.H));
+            tiles.add(new Tile(Letters.V));
+    	}
+
+        tiles.add(new Tile(Letters.J));
+        tiles.add(new Tile(Letters.Q));
         tiles.add(new Tile(Letters.U));
         tiles.add(new Tile(Letters.Y));
         tiles.add(new Tile(Letters.K));
