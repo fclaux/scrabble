65104d575b507c3e367db63c7ca5e5b17cd8e9ac
Hashual <145881009+Hashual@users.noreply.github.com>
2024-04-29 11:17:36 +0200

    Add basic functionality: add, remove and swap tiles.

--- src/main/java/scrabble/model/Rack.java
+++ src/main/java/scrabble/model/Rack.java
@@ -11,4 +11,24 @@ public class Rack {
         tiles = new ArrayList<>(MAX_TILES);
     }
     
+    public void addTile(Tile tile) {
+        if (tiles.size() < MAX_TILES) {
+            tiles.add(tile);
+        }
+    }
+
+    public void removeTile(int index) {
+        if (index >= 0 && index < tiles.size()) {
+            tiles.remove(index);
+        }
+    }
+
+    public void swapTiles(int index1, int index2) {
+        if (index1 >= 0 && index1 < tiles.size() && index2 >= 0 && index2 < tiles.size()) {
+            Tile temp = tiles.get(index1);
+            tiles.set(index1, tiles.get(index2));
+            tiles.set(index2, temp);
+        }
+    }
+    
 }
