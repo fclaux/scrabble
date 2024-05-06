545afd4d73e3d2cc569b3d8f554b21aae4db7ca7
Hashual <145881009+Hashual@users.noreply.github.com>
2024-05-06 17:40:08 +0200

    add : class scrabbleTest to make test

---
+++ src/main/java/scrabble/application/ScrabbleTest.java
@@ -0,0 +1,82 @@
+package scrabble.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import scrabble.gui.Console;
+import scrabble.model.*;
+
+public class ScrabbleTest {
+	
+	public static final int STOP_VALUE = -1;
+	
+	public static void main(String[] args) {
+		Console.separator();
+		Console.message("|   Bienvenue dans notre magnifique jeu de scrabble !   |");
+		Console.message("|   développé par Maxence                               |");
+		Console.message("|   et par Dorian                                       |");
+		Console.message("|   et par Florian                                      |");
+		Console.separator();
+		
+		GameBoard gameBoard = new GameBoard();
+		BagOfTiles bagOfTiles = new BagOfTiles();
+		Player player1 = new Player("Dorian");
+		Scanner scanner = new Scanner(System.in);
+		
+		Console.title("  La grille");
+		Console.message(gameBoard.showGameBoard());
+		
+		Console.title("  On initialise le sac de jeton et on le mélange");
+		showRemainingTilesInBag(bagOfTiles);
+		Console.message(bagOfTiles.display());
+		Console.message("On mélange...");
+		bagOfTiles.shuffle();
+		Console.message(bagOfTiles.display());
+		
+		
+		Console.message("Le joueur pioche");
+		player1.draw(bagOfTiles);
+		
+		showRemainingTilesInBag(bagOfTiles);
+		
+		
+		Console.message("Rack J1 :");
+		Console.message(player1.getRack().display());
+		
+		List<Integer> indices = playerChooseTileForChange(scanner);
+        
+		player1.exchangeTiles(bagOfTiles, indices);
+		showRemainingTilesInBag(bagOfTiles);
+		
+		Console.message("Rack J1 :");
+		Console.message(player1.getRack().display());
+		
+	}
+
+	private static List<Integer> playerChooseTileForChange(Scanner scanner) {
+		Console.title("  Pour remplacez des éléments, entrez les indices un à un puis écrivez -1 pour arrêter");
+		int input;
+		List<Integer> indices = new ArrayList<>();
+        do {
+        	Console.message("Entrez la place de l'élément que vous voulez retirez : ");
+            input = scanner.nextInt();
+		
+			if ((input < 1 || input > Rack.MAX_TILES)&&(input!=STOP_VALUE)) {
+				Console.message("ERREUR, veuillez rentrer un nombre entre 1 et "+ Rack.MAX_TILES +" : ");
+			}
+			else if (indices.contains(input-1)) {
+				Console.message("ERREUR, veuillez rentrer un nombre différents de ceux déja entrés  : ");
+			}
+			else {
+            	indices.add(input-1);
+            }
+        } while (input != STOP_VALUE);
+		return indices;
+	}
+
+	private static void showRemainingTilesInBag(BagOfTiles bagOfTiles) {
+		Console.message("Il reste "+bagOfTiles.getRemainingTilesCount()+" jetons dans la pioche !");
+	}
+
+}
