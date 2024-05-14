3dc012c204b28399897d7e981edbee2bcf6a7719
Florian Claux <florian.claux@gmail.fr>
2024-05-14 21:06:15 +0200

    add : add CellTest + refacto Cell

---
+++ src/test/java/scrabble/CellTest.java
@@ -0,0 +1,38 @@
+package scrabble;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import scrabble.model.Cell;
+import scrabble.model.Effects;
+import scrabble.model.Letters;
+import scrabble.model.Tile;
+
+class CellTest {
+	
+	private Cell cell;
+	
+	@BeforeEach
+	void setUp() {
+        cell = new Cell(Effects.NONE);
+    }
+	
+	@Test
+	void cell_is_empty_at_the_beginning() {
+		assertTrue(cell.isEmpty());
+	}
+	
+	@Test
+	void add_and_remove_tile_in_cell() {
+		Tile tile = new Tile(Letters.A);
+		cell.setTile(tile);
+		assertFalse(cell.isEmpty());
+		cell.removeTile();
+		assertTrue(cell.isEmpty());
+		cell.removeTile();
+		assertTrue(cell.isEmpty());
+	}
+	
+}
