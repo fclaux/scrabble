66efb51ae1b8da65679f1190c89050f418346312
Maxence Vincent <maxvince87@gmail.com>
2024-05-16 11:42:30 +0200

    add : class TileTest

---
+++ src/test/java/scrabble/TileTest.java
@@ -0,0 +1,30 @@
+package scrabble;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import scrabble.model.*;
+
+public class TileTest {
+
+	Tile tile;
+	
+	@BeforeEach
+	void setUp() {
+		tile = new Tile(Letters.M);
+	}
+	
+	@Test
+	void get_and_set_Letter() {
+		Letters tile_letters_before;
+		Letters tile_letters_after;
+		
+		tile_letters_before = tile.getLetter();
+		tile.setLetter(Letters.F);
+		tile_letters_after = tile.getLetter();
+		assertThat(tile_letters_before).isEqualTo(Letters.M);
+		assertThat(tile_letters_after).isNotEqualTo(tile_letters_before);
+	}
+}
