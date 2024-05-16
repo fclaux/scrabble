5cbbd38fb5135ab1c32e0f51a69062b01e572d0b
Maxence Vincent <maxvince87@gmail.com>
2024-05-16 11:33:30 +0200

    add : class PlayerTest

---
+++ src/test/java/scrabble/PlayerTest.java
@@ -0,0 +1,45 @@
+package scrabble;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import scrabble.model.*;
+
+public class PlayerTest {
+
+	Player player;
+	BagOfTiles bagOfTiles;
+	
+	@BeforeEach
+	void setUp() {
+		player = new Player("Florian");
+		bagOfTiles = new BagOfTiles();
+		bagOfTiles.shuffle();
+	}
+	
+	@Test
+	void get_and_set_score() {
+		int player_score;
+		
+		player.setScore(130);
+		player_score = player.getScore();
+		assertThat(player_score).isEqualTo(130);
+	}
+	
+	@Test
+	void get_name() {
+		String player_name;
+		
+		player_name = player.getName();
+		assertThat(player_name).isEqualTo("Florian");
+	}
+	
+	
+	@Test
+	void draw_and_exchangeTiles() {
+		//TODO
+	}
+}
