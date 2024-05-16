4ff6b528a4f378599ed78d7c23bd67ba3718c357
Dorian Loizeau <loizeau.dorian05@gmail.com>
2024-05-16 09:38:42 +0200

    Feat : add function to put a tile on the board

--- src/main/java/scrabble/application/GameMaster.java
+++ src/main/java/scrabble/application/GameMaster.java
@@ -10,6 +10,7 @@ import scrabble.model.GameBoard;
 import scrabble.model.Player;
 import scrabble.model.Rack;
 import scrabble.model.Tile;
+import scrabble.util.IndexOutOfRackException;
 
 public class GameMaster {
 	
@@ -28,7 +29,7 @@ public class GameMaster {
 	public void start() {
 		Scanner scanner = new Scanner(System.in);
 				
-		Console.message(gameBoard.showGameBoard());
+		Console.message(getGameBoard().showGameBoard());
 				
 		this.bagOfTiles.shuffle();
 				
@@ -67,6 +68,20 @@ public class GameMaster {
 			}
 		} while (input != STOP_VALUE);
 		return indices;
+	}
+	
+	public Tile putLetterFromRackToGameBoard(Rack rack, int rackIndex, int ligne, int colonne) throws IndexOutOfRackException {
+        Tile tileToPut = rack.removeTile(rackIndex);
+        if (tileToPut != null) {
+            getGameBoard().addTile(tileToPut, ligne, colonne);
+        } else {
+        	throw new IndexOutOfRackException("Invalid "+ rackIndex + " Index");
+        }
+        return tileToPut;
+    }
+
+	public GameBoard getGameBoard() {
+		return gameBoard;
 	}
 	
 	
