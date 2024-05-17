4c223a96404a3e559f529f17428517d9870fa250
Dorian Loizeau <loizeau.dorian05@gmail.com>
2024-05-17 02:24:08 +0200

    Feat : add askInt and askString methods

--- src/main/java/scrabble/gui/Console.java
+++ src/main/java/scrabble/gui/Console.java
@@ -1,5 +1,9 @@
 package scrabble.gui;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Console {
 	
 	private Console() {
@@ -10,7 +14,8 @@ public class Console {
 	}
 
 	public static final String SEPARATOR_LINE = "-------------------------------------------------------------";
-
+	private static final Scanner scanner = new Scanner(System.in);
+	
 	public static void title(String text) {
 		separator();
 		message(text);
@@ -20,5 +25,47 @@ public class Console {
 	public static void separator() {
 		message(SEPARATOR_LINE);
 	}
+	
+    public static String askString(String question) {
+        Console.message(question);
+
+        String response = null;
+        while (response == null) {
+            try {
+                response = scanner.nextLine();
+            } catch (InputMismatchException e) {
+                Console.message("Veuillez saisir une valeur");
+                scanner.next();
+            } catch (NoSuchElementException e) {
+                Console.message("Veuillez saisir une valeur");
+                scanner.next();
+            }
+        }
+        
+        return response;
+        
+    }
+    public static Integer askInt(Integer min, Integer max) {
+        Integer response = null;
+        while (response == null) {
+            try {
+                response = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                Console.message("Veuillez saisir un nombre");
+                scanner.next();
+            } catch (NoSuchElementException e) {
+                Console.message("Veuillez saisir un nombre");
+                scanner.next();
+            }
+
+            if (response < min || response > max) {
+                Console.message("Saisie incorrect veuillez réessayer");
+                response = null;
+            }
+        }
+
+
+        return response;
+    }
 }
 
