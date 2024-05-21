945cf28216b59e9572f9d414684ba4648bd80f58
Florian Claux <florian.claux@etu.unilim.fr>
2024-05-21 14:45:46 +0200

    feat:add lineReturn, space, askString, askInt, askJokerLetter in Console

--- src/main/java/scrabble/gui/Console.java
+++ src/main/java/scrabble/gui/Console.java
@@ -1,24 +1,91 @@
 package scrabble.gui;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Console {
+	
+	public static final String SEPARATOR_LINE = "-------------------------------------------------------------"; 
+	
+	private static final Scanner scanner = new Scanner(System.in);
 	
 	private Console() {
 	}
 
-	public static void message(String text) {
-		System.out.println(text);
+	public static void message(String text, boolean withLineReturn) {
+	    if (withLineReturn) {
+	        System.out.println(text);
+	    } else {
+	        System.out.print(text);
+	    }
 	}
 
-	public static final String SEPARATOR_LINE = "-------------------------------------------------------------";
-
+	
+	
 	public static void title(String text) {
 		separator();
-		message(text);
+		message(text, true);
 		separator();
 	}
 	
+	public static void space() {
+		message(" ", false);
+	}
+	
 	public static void separator() {
-		message(SEPARATOR_LINE);
+		message(SEPARATOR_LINE, true);
+	}
+	
+    public static String askString(String question) {
+        Console.message(question, true);
+
+        String response = null;
+        while (response == null) {
+            try {
+                response = scanner.nextLine();
+            } catch (NoSuchElementException e) {
+                Console.message("Veuillez saisir une valeur", true);
+                scanner.next();
+            } 
+        }
+        
+        return response;
+        
+    }
+    public static Integer askInt(Integer min, Integer max) {
+        Integer response = null;
+        while (response == null) {
+            try {
+                response = scanner.nextInt();
+                scanner.nextLine();
+            } catch (NoSuchElementException e) {
+                Console.message("Veuillez saisir un nombre", true);
+                scanner.next();
+            } 
+
+            if (response < min || response > max) {
+                Console.message("Saisie incorrect veuillez réessayer", true);
+                response = null;
+            }
+        }
+        return response;
+    }
+    
+    public static char askJokerLetter() {
+        Console.message("Vous avez joué un joker. Veuillez saisir la lettre que vous souhaitez attribuer au joker:", true);
+        String input;
+        do {
+            input = Console.askString("Lettre pour le joker: ").toUpperCase();
+            if (input.length() == 1 && Character.isLetter(input.charAt(0))) {
+                return input.charAt(0);
+            } else {
+                Console.message("Veuillez saisir une seule lettre valide.", true);
+            }
+        } while (true);
+    }
+
+	public static void lineReturn() {
+		Console.message("", true);
 	}
 }
 
