26cb2ca94434130cc48464ba9b83f377ffc67d15
Maxence Vincent <maxvince87@gmail.com>
2024-05-21 15:03:24 +0200

    add : class ConsoleBagOfTilesView in scrabble.view.console

---
+++ src/main/java/scrabble/view/console/ConsoleBagOfTilesView.java
@@ -0,0 +1,19 @@
+package scrabble.view.console;
+
+import scrabble.gui.Console;
+import scrabble.model.BagOfTiles;
+import scrabble.model.Tile;
+import scrabble.view.interfaces.BagOfTilesView;
+
+public class ConsoleBagOfTilesView implements BagOfTilesView{
+
+	@Override
+	public void display(BagOfTiles bagOfTiles) {
+		ConsoleTileView tileView = new ConsoleTileView();
+		for (Tile tile : bagOfTiles.getTiles()) {
+            tileView.display(tile);
+            Console.space();
+        }
+		Console.lineReturn();
+	}
+}
