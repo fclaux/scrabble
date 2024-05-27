ce3411f944d0a4bb7a84d53b4c40e4e359e683c7
Dorian Loizeau <loizeau.dorian05@gmail.com>
2024-05-27 11:05:33 +0200

    fix : add throw declaration for IndexOutOfBoardException

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -1,11 +1,12 @@
 package scrabble.application;
 
 import scrabble.controller.ConsoleGameController;
+import scrabble.util.IndexOutOfBoardException;
 
 public class ScrabbleApplicationConsole {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IndexOutOfBoardException {
 		ConsoleGameController gameController = new ConsoleGameController();
 		
 		gameController.startGame();
