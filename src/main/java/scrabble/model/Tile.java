d9ca96012c6b8ab4f362e2f911b3b0d9f1e60744
Florian Claux <florian.claux@gmail.fr>
2024-05-30 18:17:39 +0200

    feat : add equals and hashcode in Tile

--- src/main/java/scrabble/model/Tile.java
+++ src/main/java/scrabble/model/Tile.java
@@ -1,5 +1,7 @@
 package scrabble.model;
 
+import java.util.Objects;
+
 public class Tile {
 	
 	
@@ -25,5 +27,22 @@ public class Tile {
 	
 	public boolean isJoker() {
 		return this.isJoker;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(isJoker, letter);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tile other = (Tile) obj;
+		return isJoker == other.isJoker && letter == other.letter;
 	}
 }
