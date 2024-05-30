36a81cefb663ed74f04d2e7851be3c7b8406fd86
Florian Claux <florian.claux@gmail.fr>
2024-05-30 20:41:46 +0200

    test : update model Test

--- src/test/java/scrabble/TileTest.java
+++ src/test/java/scrabble/TileTest.java
@@ -1,6 +1,10 @@
 package scrabble;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +16,7 @@ class TileTest {
 	Tile tile;
 	
 	@BeforeEach
-	void setUp() {
+	void set_up() {
 		tile = new Tile(Letters.M);
 	}
 	
@@ -27,4 +31,37 @@ class TileTest {
 		assertThat(tile_letters_before).isEqualTo(Letters.M);
 		assertThat(tile_letters_after).isNotEqualTo(tile_letters_before);
 	}
+	
+	@Test
+	void is_not_a_joker() {
+		assertFalse(tile.isJoker());
+	}
+	
+	@Test
+	void is_a_joker() {
+		Tile joker = new Tile(Letters.JOKER);
+		assertTrue(joker.isJoker());
+	}
+	
+	@Test
+    void test_equals_and_hash_code() {
+        Tile tile1 = new Tile(Letters.A);
+        Tile tile2 = new Tile(Letters.A);
+        Tile tile3 = new Tile(Letters.B);
+        assertTrue(tile1.equals(tile1));
+
+        assertTrue(tile1.equals(tile2));
+        assertEquals(tile1.hashCode(), tile2.hashCode());
+
+        assertFalse(tile1.equals(tile3));
+        assertNotEquals(tile1.hashCode(), tile3.hashCode());
+
+        Tile joker1 = new Tile(Letters.JOKER);
+        Tile joker2 = new Tile(Letters.JOKER);
+        assertTrue(joker1.equals(joker2));
+        assertEquals(joker1.hashCode(), joker2.hashCode());
+
+        assertFalse(tile1.equals("A"));
+        assertFalse(tile1.equals(null));
+    }
 }
