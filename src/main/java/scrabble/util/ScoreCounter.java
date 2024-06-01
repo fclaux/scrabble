9613823ad44ef8ad486b2eefb693554f8e013564
Hashual <145881009+Hashual@users.noreply.github.com>
2024-06-01 21:48:15 +0200

    Add : add the scrabble to the score

--- src/main/java/scrabble/util/ScoreCounter.java
+++ src/main/java/scrabble/util/ScoreCounter.java
@@ -1,8 +1,8 @@
 package scrabble.util;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import scrabble.controller.ConsoleGameController;
 import scrabble.gui.Console;
 import scrabble.model.Cell;
 import scrabble.model.GameBoard;
@@ -10,64 +10,23 @@ import scrabble.model.Move;
 import scrabble.model.Tile;
 
 public class ScoreCounter {
+	public static final int SCRABBLE = 50;
 
     public int calculateScoreForMoves(List<Move> moves, GameBoard gameBoard) throws IndexOutOfBoardException {
-        List<List<Cell>> words = findWordsFromMoves(moves, gameBoard);
+        List<List<Cell>> words = ConsoleGameController.findWordsFromMoves(moves, gameBoard);
         int totalScore = 0;
 
         for (List<Cell> word : words) {
             totalScore += calculateScoreForWord(word);
         }
-
+        Console.message(""+moves.size(), false);
+        if (moves.size() == 7) {
+        	totalScore += SCRABBLE;
+        }
         return totalScore;
     }
 
-    private List<List<Cell>> findWordsFromMoves(List<Move> moves, GameBoard gameBoard) throws IndexOutOfBoardException {
-        List<List<Cell>> words = new ArrayList<>();
-
-        for (Move move : moves) {
-            List<Cell> horizontalWord = getWord(move, gameBoard, true);
-            if (horizontalWord.size() > 1 && !words.contains(horizontalWord)) {
-                words.add(horizontalWord);
-            }
-
-            List<Cell> verticalWord = getWord(move, gameBoard, false);
-            if (verticalWord.size() > 1 && !words.contains(verticalWord)) {
-                words.add(verticalWord);
-            }
-        }
-        Console.message(""+words, true);
-        Console.message("moves"+moves,false);
-
-        return words;
-    }
-
-    private List<Cell> getWord(Move move, GameBoard gameBoard, boolean isHorizontal) throws IndexOutOfBoardException {
-        List<Cell> word = new ArrayList<>();
-        int row = move.row();
-        int col = move.col();
-
-        while ((isHorizontal ? col > 0 : row > 0) && 
-               !gameBoard.cell(isHorizontal ? row : row - 1, isHorizontal ? col - 1 : col).isEmpty()) {
-            if (isHorizontal) {
-                col--;
-            } else {
-                row--;
-            }
-        }
-
-        while ((isHorizontal ? col < GameBoard.SIZE_GRID : row < GameBoard.SIZE_GRID) && 
-               !gameBoard.cell(row, col).isEmpty()) {
-            word.add(gameBoard.cell(row, col));
-            if (isHorizontal) {
-                col++;
-            } else {
-                row++;
-            }
-        }
-
-        return word;
-    }
+    
 
     private int calculateScoreForWord(List<Cell> cells) {
         int totalScore = 0;
@@ -87,7 +46,7 @@ public class ScoreCounter {
                     tileScore *= 2;
                     break;
                 case TRIPLE_LETTER:
-                    tileScore *= 3;
+                  )  tileScore *= 3;
                     break;
                 case DOUBLE_WORD:
                     wordMultiplier *= 2;
@@ -103,6 +62,7 @@ public class ScoreCounter {
             }
 /*/
             totalScore += tileScore;
+
         }
 
         return totalScore * wordMultiplier;
