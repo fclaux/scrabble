607f433c45da207e430dcfe9c1c7b0ed0a9c30cc
Dorian Loizeau <loizeau.dorian05@gmail.com>
2024-06-01 22:58:59 +0200

    Add : multipliers for words and tiles

--- src/main/java/scrabble/util/ScoreCounter.java
+++ src/main/java/scrabble/util/ScoreCounter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import scrabble.controller.ConsoleGameController;
 import scrabble.gui.Console;
 import scrabble.model.Cell;
+import scrabble.model.Effects;
 import scrabble.model.GameBoard;
 import scrabble.model.Move;
 import scrabble.model.Tile;
@@ -19,14 +20,13 @@ public class ScoreCounter {
         for (List<Cell> word : words) {
             totalScore += calculateScoreForWord(word);
         }
-        Console.message(""+moves.size(), false);
         if (moves.size() == 7) {
         	totalScore += SCRABBLE;
         }
         return totalScore;
     }
 
-    
+
 
     private int calculateScoreForWord(List<Cell> cells) {
         int totalScore = 0;
@@ -40,27 +40,40 @@ public class ScoreCounter {
             } else {
                 tileScore = tile.letter().value();
             }
-/*/
-            switch (cell.effect()) {
+        	
+        		switch (cell.effect()) {
+                
                 case DOUBLE_LETTER:
                     tileScore *= 2;
+                    Console.message("Lettre double", true);
+                    cell.setEffect(Effects.NONE);
                     break;
                 case TRIPLE_LETTER:
-                  )  tileScore *= 3;
+                    tileScore *= 3;
+                    Console.message("Lettre tripple", true);
+                    cell.setEffect(Effects.NONE);
                     break;
                 case DOUBLE_WORD:
                     wordMultiplier *= 2;
+                    Console.message("Mot double", true);
+                    cell.setEffect(Effects.NONE);
                     break;
                 case STARS:
                     wordMultiplier *= 2;
+                    Console.message("Étoile", true);
+                    cell.setEffect(Effects.NONE);
                     break;
                 case TRIPLE_WORD:
                     wordMultiplier *= 3;
+                    Console.message("Mot tripple", true);
+                    cell.setEffect(Effects.NONE);
                     break;
                 default:
                     break;
-            }
-/*/
+            
+        	}
+            
+
             totalScore += tileScore;
 
         }
